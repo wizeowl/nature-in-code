3b24bb31a55c8f9c3d1c977306d6d38fdeda635f
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2018-01-21 00:12:07 +0100

    implemented multi genetic drifts

---
+++ multi-genetic-drift.js
@@ -0,0 +1,34 @@
+var N = 1000;
+var generations = 1200;
+
+function geneticDrift() {
+    var p = .5;
+    var data = [];
+
+    function next_generation() {
+        var draws = 2 * N;
+        var a1 = 0;
+        var a2 = 0;
+        for (var i = 0; i < draws; i++) {
+            if (Math.random() <= p) a1++;
+            else a2++;
+        }
+        p = a1 / draws;
+        data.push(p);
+    }
+
+    for (var i = 0; i < generations; i++) {
+        next_generation();
+    }
+
+    return data;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var data = [];
+    for (var i = 0; i < 4; i++) {
+        data.push(geneticDrift());
+    }
+
+    draw_line_chart(data, "Generation", "p", ["Population size", N, "Generations:", generations]);
+}, false);
