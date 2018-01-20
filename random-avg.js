025f0f5bf03b4f63e217b2c20f2d554ddcc6f7e7
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2018-01-20 22:28:13 +0100

    implemented genetic drift

---
+++ random-avg.js
@@ -0,0 +1,12 @@
+var repeats = 100000000;
+var sum = 0;
+var average = 0;
+
+console.time();
+for (var i = 0; i < repeats; i++) {
+  sum += Math.random();
+}
+
+average = sum / repeats;
+console.log(average);
+console.timeEnd();
