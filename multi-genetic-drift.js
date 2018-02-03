4663f413b8143acd5cea6362bb9b81358a166a47
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2018-02-03 22:39:24 +0100

    implemented effective population size

--- multi-genetic-drift.js
+++ multi-genetic-drift.js
@@ -8,8 +8,9 @@ function GeneticDrift(inN, inGgenerations, inP) {
 
     simulations = [];
 
-    simulate = function (reps) {
-        debugger
+    populationSizes = [];
+
+    simulate = (reps) => {
         for (let i = 0; i < reps; i++) {
             this.simulations.push(this.drift(this.N, this.generations, this.p));
         }
@@ -20,16 +21,18 @@ function GeneticDrift(inN, inGgenerations, inP) {
     drift = function (N, generations, p) {
         let data = [p];
         let nextP = p;
+        let nextN = N;
 
         for (let i = 0; i < generations; i++) {
-            nextP = this.nextGeneration(nextP, N);
+            [nextP, nextN] = this.nextGeneration(nextP, nextN);
             data.push(nextP);
+            this.populationSizes.push(nextN);
         }
 
         return data;
     }
 
-    nextGeneration = function (p, N) {
+    nextGeneration = (p, N)=> {
         let draws = 2 * N;
         let a1 = 0;
         let a2 = 0;
@@ -41,22 +44,26 @@ function GeneticDrift(inN, inGgenerations, inP) {
         }
 
         // return the next ratio
-        return a1 / draws;
+        return [a1 / draws, N];
     }
 
-    clear = function () {
+    clear = () => {
         this.simulations = [];
     }
-    
+
+    // there must be protection against division by zero but i'm feeling sloppy
+    effectivePopulationSize = () => {
+        return Math.round(this.populationSizes.length / this.populationSizes.reduce((accumulator, current) => accumulator + 1 / current, 0));
+    }
+
     return this;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     let geneticDrift = GeneticDrift();
-    console.log(geneticDrift);
 
     draw_line_chart(geneticDrift.simulate(5), "Generation", "p", [
-        "Population size", geneticDrift.N,
+        "Eff. Population size", geneticDrift.effectivePopulationSize(),
         "Generations:", geneticDrift.generations
     ]);
 
